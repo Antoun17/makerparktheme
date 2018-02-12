3f5216f18feb794eb54f03a0344bd065ccfb2872
Antou Sawires <antounsawires@gmail.com>
2018-02-12 12:31:29 -0500

    new navbar

--- js/active.js
+++ js/active.js
@@ -1,20 +1,9 @@
-var navlnksuno = document.querySelectorAll(".nav-link a");
-       Array.prototype.map.call(navlnksuno, function(item) {
-
-           item.addEventListener("click", function(e) {
-
-               var navlnks = document.querySelectorAll(".nav-link a");
-
-               Array.prototype.map.call(navlnks, function(item) {
-
-                   if (item.parentNode.className == "active" || item.parentNode.className == "active open" ) {
-
-                       item.parentNode.className = "";
-
-                   }
-
-               });
-
-               e.currentTarget.parentNode.className = "active";
-           });
-       });
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
