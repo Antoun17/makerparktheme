b65ec4649c2535bbd1449cc4f118ba6a17d400a7
Antou Sawires <antounsawires@gmail.com>
2018-02-12 14:01:58 -0500

    cards

--- js/active.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($) {
-  $(".nav-link").on("click", function(){
-     $(".nav-item").find(".active").removeClass("active");
-     $(this).parent().addClass("active");
-  })
-});
