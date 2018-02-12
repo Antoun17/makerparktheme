2f8ddbe45855e0c38179e479fc49a3a90bff54a3
Antou Sawires <antounsawires@gmail.com>
2018-02-12 13:16:33 -0500

    navbar active working

--- js/active.js
+++ js/active.js
@@ -1,12 +1,6 @@
 jQuery(document).ready(function($) {
-
-var header = document.getElementById("navbarNavDropdown");
-var btns = header.getElementsByClassName('nav-item');
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-    })
-  }
+  $(".nav a").on("click", function(){
+     $(".nav").find(".active").removeClass("active");
+     $(this).parent().addClass("active");
+  })
 });
